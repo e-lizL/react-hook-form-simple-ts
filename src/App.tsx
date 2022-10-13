84c3892dce2bd08c8654284295c241e6780e82d7
Elizabeth Lands <elizabeth_lands@hotmail.com>
2022-10-13 12:32:50 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IFormInput {
+  name: string;
+  email: string;
+  message: string;
 }
 
-export default App;
+export default function ReactHookFormSimpleTS() {
+  const [thanksMessage, setThanksMessage] = useState(false);
+  const { register, handleSubmit } = useForm<IFormInput>();
+
+  const mySubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+    setThanksMessage(true);
+  };
+  return (
+    <>
+      <h1>Simple React Hook Form Using TS</h1>
+
+      <form onSubmit={handleSubmit(mySubmit)}>
+        <div>
+          <label>name</label>
+          <input {...register("name")}></input>
+        </div>
+
+        <div>
+          <label>email</label>
+          <input {...register("email")}></input>
+        </div>
+
+        <div>
+          <label>message</label>
+          <input {...register("message")}></input>
+        </div>
+
+        <input type="submit" />
+      </form>
+
+      {thanksMessage && (
+        <h2>Thanks for contacting us, we'll get back to you shortly.</h2>
+      )}
+    </>
+  );
+}
